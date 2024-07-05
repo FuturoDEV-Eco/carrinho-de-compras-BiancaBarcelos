7250f8348ce44db778053ffddc155ecca6981fc9
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-07-04 22:04:01 -0300

    feat: listar produto com detalhe

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -34,6 +34,7 @@ class ProductController{
         }
     }
 
+    //Listar todos os Produtos
     async listarTodos(request, response) {
         try {
             const produtos = await conexao.query("select * from products")
@@ -42,6 +43,27 @@ class ProductController{
             response.status(500).json({ mensagem: 'Não possivel listar os produtos' })
         }
     }
+
+    //Listar detalhes do Produto
+    async listarProduto(request, response) {
+        try {
+            const idProduto = request.params.id
+            const produto = await conexao.query(`
+                SELECT p.name, p.amount, p.color, p.voltage, p.description, c.name AS category_name
+                FROM products p
+                INNER JOIN categories c ON p.category_id = c.id 
+                WHERE p.id = ${idProduto}`)
+            if (produto.rows.length > 0) {
+                response.status(200).json(produto.rows[0])
+            }else {
+                response.status(404).json({ mensagem: 'Produto não encontrado' })
+            }
+        } catch (error) {
+            response.status(500).json({ mensagem: "Erro ao carregar o produto" })
+        }
+    }
 }
+
+
 
 module.exports = new ProductController()
