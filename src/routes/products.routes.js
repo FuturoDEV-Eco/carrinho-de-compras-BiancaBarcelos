892ba886feb29c125d8de2d8d6b2ebb607d4c63b
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-07-04 21:25:27 -0300

    feat: listar produtos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,5 +4,6 @@ const ProductController = require('../controllers/ProductController')
 const productsRoutes = new Router()
 
 productsRoutes.post("/cadastrar", ProductController.criar)
+productsRoutes.get("/listar", ProductController.listarTodos)
 
 module.exports = productsRoutes
